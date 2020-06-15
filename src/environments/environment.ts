71d034faad3ad99e8bd0df3fb5f0177fd19995d8
manigedit <1manish22@gmail.com>
2020-06-16 04:09:36 +0530

    feat(core+login): add Core Module and Login functiinality

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,20 @@
 
 export const environment = {
   production: false,
+<<<<<<< HEAD
   fineractPlatformTenantId: 'mobile',  // For connecting to server running elsewhere update the tenant identifier
   baseApiUrl: 'https://mobile.mifos.io',  // For connecting to server running elsewhere update the base API URL
   apiProvider: '/fineract-provider/api',
   apiVersion: '/v1',
   serverUrl: 'https://mobile.mifos.io/fineract-provider/api/v1',
   selfServiceRoleId: 2
+=======
+  fineractPlatformTenantId: 'default',  // For connecting to server running elsewhere update the tenant identifier
+  baseApiUrl: 'https://staging.mifos.io',  // For connecting to server running elsewhere update the base API URL
+  apiProvider: '/fineract-provider/api',
+  apiVersion: '/v1',
+  serverUrl: 'https://staging.mifos.io/fineract-provider/api/v1'
+>>>>>>> feat(core+login): add Core Module and Login functiinality
 };
 
 /*
